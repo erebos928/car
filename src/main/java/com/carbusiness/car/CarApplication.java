04da4ddd8c2aa19e81720cc55d0950383ba09278
behrooz <shahin.behrooz@gmail.com>
2023-08-01 15:38:35 -0600

    datasource auto disable

--- src/main/java/com/carbusiness/car/CarApplication.java
+++ src/main/java/com/carbusiness/car/CarApplication.java
@@ -1,9 +1,12 @@
 package com.carbusiness.car;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CarApplication {
 
 	public static void main(String[] args) {
